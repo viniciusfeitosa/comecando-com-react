99d95d0b10afc29482fd6164d7b6e583ec4853b4
Vinicius Pacheco <vfpweb@gmail.com>
2016-12-04 18:41:16 -0300

    App react with es6

---
+++ app/services/GitHubUser.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+
+const GitHubUser = {
+    getByUsername(username) {
+        return axios.get(`http://api.github.com/users/${username}`);
+    },
+
+    getReposByUsername(username) {
+        return axios.get(`http://api.github.com/users/${username}/repos`);
+    },
+};
+
+
+export default GitHubUser;
